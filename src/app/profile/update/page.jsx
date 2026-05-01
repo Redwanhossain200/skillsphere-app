16bc007d42bdf056a3fb1a3b6eb1dd710559dfed
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-05-01 21:17:54 +0600

    profile pages added

---
+++ src/app/profile/update/page.jsx
@@ -0,0 +1,23 @@
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import UpdateProfileForm from '@/components/UpdateProfileForm';
+
+export default async function UpdateProfilePage() {
+  const headerData = await headers();
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/api/auth/get-session`,
+    {
+      headers: { cookie: headerData.get('cookie') || '' },
+    },
+  );
+
+  const session = await res.json();
+
+  if (!session) {
+    redirect('/login');
+  }
+
+  const { user } = session;
+
+  return <UpdateProfileForm user={user} />;
+}
