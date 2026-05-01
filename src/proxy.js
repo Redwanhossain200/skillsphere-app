4aae15473c084dbd98441dedd85ed84dbce0249b
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-05-01 21:34:28 +0600

    add cart context, authentication client, and proxy added

---
+++ src/proxy.js
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+
+export async function proxy(request) {
+  const isAuthRoute =
+    request.nextUrl.pathname.startsWith('/login') ||
+    request.nextUrl.pathname.startsWith('/register');
+  const isProtectedRoute =
+    request.nextUrl.pathname.startsWith('/profile/update') ||
+    (request.nextUrl.pathname.startsWith('/courses/') &&
+      request.nextUrl.pathname.length > 9);
+
+  try {
+    const response = await fetch(
+      `${request.nextUrl.origin}/api/auth/get-session`,
+      {
+        headers: {
+          cookie: request.headers.get('cookie') || '',
+        },
+      },
+    );
+
+    const session = await response.json();
+
+    if (isProtectedRoute && !session) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (isAuthRoute && session) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } catch (error) {
+    console.error('Proxy session check failed:', error);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/profile/:path*', '/courses/:id*', '/login', '/register'],
+};
